ad22d2b5eea4c06a89b9c435d42add7e7ddb6927
LeonOfc202 <hhbb65202@gmail.com>
2022-10-27 10:11:56 +0800

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -12,14 +12,14 @@ Pengen Yang Model Script Bot Seperti Ini Bro?
 Pengen yang udah di recode oleh developer *Script Private*?
 Script bot ini dijual bila kalian mau bisa chat developer
 
-wa.me/6282127487538'
+wa.me/6287760550924'
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://Instagram.com/Dmssrdn",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'Kenzo-Botz - MultiDevice',
+    title: 'Nana Chan - MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
